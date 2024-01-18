c7551bedbad7e3583b9993ab9a2113e61f9e8d08
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-01-18 20:18:32 +0800

    Commiting

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+if __name__ == "main":
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
