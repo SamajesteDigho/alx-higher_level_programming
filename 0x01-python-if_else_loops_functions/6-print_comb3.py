d8f06083a8713edb27d58f9f87ef43bedcc47050
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-01-18 08:07:29 +0800

    Project 2 Exo 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(0, 10):
     for j in range(i, 10):
         if i != j:
-            print(f"{i}{j}, ", end='')
+            print("{}{}, ".format(i, j), end='')
             if i == 8 and j == 9:
                 break
 print("89")
