ab555f69cb2dd1e597218f58cd8282348a274d6d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-01-26 14:27:04 +0800

    Commiting

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return 0
     cpy = "{}.".format(roman_string)
     cvt = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     res = []
-    if roman_string or type(roman_string) is type("HOLLA"):
+    if roman_string:
         for i, x in enumerate(cpy):
             if x == ".":
                 res.append(0)
