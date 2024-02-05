77624b3532c7a048a03730bd34f06fd783009408
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-06 00:08:11 +0800

    Commiting

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,20 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end='')
             count += 1
         except IndexError as e:
-            print("{}".format(e), end='')
+            print(e.__traceback__())
         except Exception:
             print('', end='')
     print("")
     return count
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
