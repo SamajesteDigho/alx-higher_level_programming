c05841d657efe772efc29075e004ba5f828864a0
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-06 00:00:23 +0800

    Commiting

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except IndexError as e:
             print("{}".format(e), end='')
+        except Exception:
+            print('', end='')
     print("")
     return count
