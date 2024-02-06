88247ffab2ed284cc208ea932469b80fa8f28215
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-06 18:12:52 +0800

    Commiting

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    """Here is the class which defines the Square Object."""
+    """
+        Here is the class which defines the Square Object.
+    """
     def __init__(self):
+        """ Constructor for the Square Object
+        
+        Note:
+            This function is to initilize the object
+        """
         pass
