0b55df516a932af51302cff94d950aa705019090
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-06 18:52:10 +0800

    Commiting

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-class Square:
-    """Here is the class which defines the Square Object."""
+"""Square Object module.
+This module defines the Square Object class and all the functions
+with the given properties and methods
+Example:
+    Square(size, position)
+"""
 
+
+class Square:
+    """
+        Here is the class which defines the Square Object.
+    """
     def __init__(self, size=0):
-        """Here is the constructor of an instance of the Square Object
+        """ Constructor for the Square Object
+        Note:
+            This function is to initilize the object
         Args:
-            size: Size of the square instance
+            size: Size of the square
         """
         self.size = size
 
@@ -15,17 +26,12 @@ class Square:
 
     @size.setter
     def size(self, value):
-        try:
-            if type(value) is not type(0):
-                raise TypeError
-            elif value < 0:
-                raise ValueError
-            else:
-                self.__size: int = value
-        except TypeError:
-            raise TypeError("size must an integer")
-        except ValueError:
+        if type(value) is not type(0):
+            raise TypeError("size must be an integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size: int = value
 
     def area(self):
         """Returns the area of the square given its size"""
