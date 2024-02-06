d6651b3579fb448a398c4e0c77f68e0797c70c68
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-06 18:25:03 +0800

    Commiting

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Square Object module.
+This module defines the Square Object class and all the functions
+with the given properties and methods
+Example:
+    Square(size, position)
+"""
+
+
 class Square:
-    """Here is the class which defines the Square Object."""
-    def __init__(self, size):
-        """Initialization of the Square object
-        Args:
-            size: Size of the square.
+    """
+        Here is the class which defines the Square Object.
+    """
+    def __init__(self, size=0):
+        """ Constructor for the Square Object
+        Note:
+            This function is to initilize the object
         """
         self.__size = size
