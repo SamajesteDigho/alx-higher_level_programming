0a2b510d21322ee52e247ed2a19885e58b5764df
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-06 18:26:44 +0800

    Commiting

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-class Square:
-    """Here is the class which defines the Square Object."""
+# -*- coding: utf-8 -*-
+"""Square Object module.
+This module defines the Square Object class and all the functions
+with the given properties and methods
+Example:
+    Square(size, position)
+"""
 
+
+class Square:
+    """
+        Here is the class which defines the Square Object.
+    """
     def __init__(self, size=0):
-        """Here is the constructor of an instance of the Square Object
+        """ Constructor for the Square Object
+        Note:
+            This function is to initilize the object
         Args:
-            size: Size of the square instance
+            size: Size of the square
         """
         try:
             if type(size) is not type(0):
