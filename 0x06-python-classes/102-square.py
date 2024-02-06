fd90d907f3c259e7f4fa657237fc6cd7b6df2dc5
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-06 22:40:47 +0800

    Commiting

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Square Object module.
+This module defines the Square Object class and all the functions
+with the given properties and methods
+Example:
+    Square(size, position)
+"""
+
+
+class Square:
+    """
+        Here is the class which defines the Square Object.
+    """
+    def __init__(self, size=0):
+        """ Constructor for the Square Object
+        Note:
+            This function is to initilize the object
+        Args:
+            size: Size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not type(0):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the area of the square given its size"""
+        return self.size ** 2
+    
+    def __eq__(self, other):
+        """== operator taken in charge by the object"""
+        return self.area() == other.area()
+    
+    def __neq__(self, other):
+        """!= operator taken in charge by the object"""
+        return self.area() != other.area()
+    
+    def __gt__(self, other):
+        """> operator taken in charge by the object"""
+        return self.area() > other.area()
+    
+    def __ge__(self, other):
+        """>= operator taken in charge by the object"""
+        return self.area() >= other.area()
+    
+    def __lt__(self, other):
+        """< operator taken in charge by the object"""
+        return self.area() < other.area()
+    
+    def __le__(self, other):
+        """<= operator taken in charge by the object"""
+        return self.area() <= other.area()
