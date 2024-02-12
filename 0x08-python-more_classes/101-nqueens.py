8546eadecb8b7696caa50a290bdef204e11d7794
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-12 23:32:16 +0800

    Commiting

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -9,4 +9,12 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    print("Values good")
+    try:
+        N = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+    print("N is {:d}".format(N))
