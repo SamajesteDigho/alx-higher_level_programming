a470a004eea39a4b6509b50b1471289da1b6e774
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-12 21:52:48 +0800

    Commiting

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Module for the representation of the class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Here is the class Rectangle docstring
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,5 @@
+# alx-higher_level_programming
+
+# 0x08-python-more_classes
+This is the seventh project directory for more
+about classes exercises.
