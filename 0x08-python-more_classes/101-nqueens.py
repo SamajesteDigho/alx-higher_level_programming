5d36b6d49246bae455008b03f1e60ec5bb8345fd
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-12 23:27:18 +0800

    Commiting

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    The N queens game player programming
+"""
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    print("Values good")
