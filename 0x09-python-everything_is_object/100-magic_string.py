02dfec1da9ed523836d79f66833a56992a8e4903
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-15 10:33:12 +0800

    Commiting

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    string = ", ".join(["BestSchool" for _ in range(i + 1)])
-    return string
+    global i
+    return ", ".join(["BestSchool" for _ in range(i + 1)])
