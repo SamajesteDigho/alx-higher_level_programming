960321dab49f1304f12b5b881750c947a1332e5f
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-15 15:25:31 +0800

    Commiting

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,4 +14,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(int(a) + int(b))
+    result = int(a) + int(b)
+    if result == float('inf') or result == -float('inf'):
+        return 89
+    return result
