c2e70ec64340578ab8a565328296b5c0574b4a28
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-20 16:54:38 +0800

    Commiting

---
+++ 0x07-python-test_driven_development/testing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Doc """
+
+def add_integer(a=0, b=0):
+    """ Doc """
+    if a is None or (type(a) is not int and type(a) is not float):
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
