49e5948f09558725f606ec704e6c4332bfc95414
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-20 19:32:08 +0800

    Commiting

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,5 @@ def read_file(filename=""):
     """
         The function definition documentation
     """
-    with open(filename) as f:
+    with open(filename, "r") as f:
         print("{}".format(f.read()))
