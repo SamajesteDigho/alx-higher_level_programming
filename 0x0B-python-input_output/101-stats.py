36906cdd3897e58bd3aaf84b5544a84d0f4c83cb
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-21 00:55:00 +0800

    Commiting

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,18 +5,19 @@
 import sys
 
 
-count = 0
-status = {}
-size = 0
-for x in sys.stdin:
-    count += 1
-    if count % 10 == 0:
-        print("File size: {}".format(size))
-        for key in status.keys():
-            print("{}: {}".format(key, status[key]))
-    data = x.strip().split(" ")
-    size += int(data[-1])
-    if data[-2] in status.keys():
-        status[data[-2]] += 1
-    else:
-        status[data[-2]] = 1
+if __name__ == "__main__":
+    count = 0
+    status = {}
+    size = 0
+    for x in sys.stdin:
+        count += 1
+        if count % 10 == 0:
+            print("File size: {}".format(size))
+            for key in status.keys():
+                print("{}: {}".format(key, status[key]))
+        data = x.strip().split(" ")
+        size += int(data[-1])
+        if data[-2] in status.keys():
+            status[data[-2]] += 1
+        else:
+            status[data[-2]] = 1
