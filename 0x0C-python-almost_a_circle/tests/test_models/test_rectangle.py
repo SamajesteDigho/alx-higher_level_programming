5e66bc5809c9340e37864662f82a0ec9a1a5b463
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-02-28 23:50:10 +0800

    Commiting

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,17 +13,23 @@ class RectangleTestCase(unittest.TestCase):
         rec3 = Rectangle(1, 2, 3, 4)
         rec4 = Rectangle(1, 2, 3, 4, 5)
         self.assertEqual(rec1.width, 1)
+        self.assertEqual(rec1.height, 2)
         self.assertEqual(rec2.x, 3)
         self.assertEqual(rec3.y, 4)
         self.assertEqual(rec4.id, 5)
     
     def test_errors(self):
         """Test documentation here we find it"""
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError) as ctx:
             Rectangle("1", 2)
             Rectangle(1, "2")
             Rectangle(1, 2, "3")
             Rectangle(1, 2, 3, "4")
+
+            self.assertIn("width must be an integer", str(ctx.exception))
+            self.assertIn("height must be an integer", str(ctx.exception))
+            self.assertIn("x must be an integer", str(ctx.exception))
+            self.assertIn("y must be an integer", str(ctx.exception))
         
         with self.assertRaises(ValueError):
             Rectangle(-1, 2)
