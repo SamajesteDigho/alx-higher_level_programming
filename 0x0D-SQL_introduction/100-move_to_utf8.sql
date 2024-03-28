d95525c11b23208f1dfb83d9609b32e8a625899e
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-29 07:00:16 +0800

    Commiting

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Updating database properties
+USE hbtn_0c_0;
+SET NAMES utf8mb4;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY `name` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
