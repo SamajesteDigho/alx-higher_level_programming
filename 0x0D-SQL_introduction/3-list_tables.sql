56e320e6e1d6b3635eb63ce28fe696da748e810d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-29 06:07:13 +0800

    Commiting

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,2 @@
 -- Display all tables of a given database
-USE "$1";
 SHOW TABLES;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,4 @@
 -- Create table name
--- Open database
-USE "$1";
 -- Create a table
 CREATE TABLE IF NOT EXISTS first_table (
     id int,
