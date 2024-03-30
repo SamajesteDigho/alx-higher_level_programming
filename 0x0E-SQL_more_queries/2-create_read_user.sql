52fdd9016fa051f9a76f10af55ed03b4ef8fddd9
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-30 16:46:55 +0800

    Comiting Exo 1

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,7 @@
 -- Read User
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
 -- Create user
 CREATE USER user_0d_2@localhost IDENTIFIED BY "user_0d_2_pwd";
-
 -- Grant privilege
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
