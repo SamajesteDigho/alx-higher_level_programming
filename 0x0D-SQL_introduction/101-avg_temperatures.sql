4b31615b6fddf8bce1b5e7d8510f75fdea6ab15c
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-30 11:51:48 +0800

    Commiting exo 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Display Temperature
-SELECT `city`, AVG(`value`) as avg_tmp
+SELECT `city`, AVG(`value`) as avg_temp
 FROM temperatures
 GROUP BY `city`
-ORDER BY avg_tmp DESC;
+ORDER BY avg_temp DESC;
