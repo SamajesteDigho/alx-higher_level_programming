4e353a6a988668d31247bfad108d2fa8fe228c4f
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-31 00:47:57 +0800

    Commiting

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,1 +1,15 @@
--- 
+-- Create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id int,
+    name VARCHAR(256)
+);
+
+ALTER TABLE cities
+ADD CONSTRAINT fk_cities_states
+FOREIGN KEY (state_id)
+REFERENCES states(id);
