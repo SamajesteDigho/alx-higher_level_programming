3dbf79ebe0facd0e8e8bdd21264c9ede759012bd
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-03-31 00:12:58 +0800

    Commiting

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,9 @@
--- 
+-- Create database hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id int AUTO UNIQUE NOT NULL PRIMARY,
+    name VARCHAR(256) NOT NULL
+);
