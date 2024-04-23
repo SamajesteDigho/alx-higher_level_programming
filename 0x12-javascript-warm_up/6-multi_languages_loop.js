6e6fb0652ca68dd0735cecac3850f7a1ff340fda
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-23 09:44:58 +0800

    Exo6 Correction

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-let arr = [
+const arr = [
   'C is fun',
   'Python is cool',
   'JavaScript is amazing'
 ];
+let i;
 for (i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
