0de270bcdfc922b9486ea7235db4fc9374e283ca
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-24 07:59:38 +0800

    Exo 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str = '';
+    let i, j;
+    for (i = 0; i < this.height; i++) {
+      for (j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      if (i + 1 < this.height) {
+        str += '\n';
+      }
+    }
+    console.log(str);
+  }
+}
+
+module.exports = Rectangle;
