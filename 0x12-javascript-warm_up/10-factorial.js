64fa7418652822e07e19036c72e5cc7918e839bd
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-23 09:52:47 +0800

    Exo10 Correction

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let args = process.argv;
-let nb = parseInt(args[2]);
+const args = process.argv;
+const nb = parseInt(args[2]);
 
 function factorial (a) {
   if (a) {
-    if (a == 0 || a == 1) {
+    if (a === 0 || a === 1) {
       return 1;
     }
     return a * factorial(a - 1);
