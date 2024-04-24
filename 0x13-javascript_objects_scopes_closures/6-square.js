0af7267ac2410ba1fdefe864aa6316880bf142ae
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-24 08:26:14 +0800

    Exo 4 to 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    let filler, i, j;
+    let str = '';
+    if (c) {
+      filler = c;
+    } else {
+      filler = 'X';
+    }
+    for (i = 0; i < this.size; i++) {
+      for (j = 0; j < this.size; j++) {
+        str += filler;
+      }
+      if (i + 1 < this.size) {
+        str += '\n';
+      }
+    }
+    console.log(str);
+  }
+}
+
+module.exports = Square;
