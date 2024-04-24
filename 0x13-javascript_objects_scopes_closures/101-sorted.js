0f46b2fcc0ba5177309b7a9fbebb1cb79536d3a2
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-24 09:53:07 +0800

    Exo 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+const newDict = {};
+
+for (const elt in dict) {
+  if (newDict[dict[elt]]) {
+    newDict[dict[elt]].push(elt);
+  } else {
+    newDict[dict[elt]] = [elt];
+  }
+}
+
+console.log(newDict);
