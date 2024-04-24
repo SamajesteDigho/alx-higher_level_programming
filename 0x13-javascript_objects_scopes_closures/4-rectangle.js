809e245b46b76b6551ade8124d5bbd1352f388e6
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-24 08:04:59 +0800

    Exo 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,37 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str = '';
+    let i, j;
+    for (i = 0; i < this.height; i++) {
+      for (j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      if (i + 1 < this.height) {
+        str += '\n';
+      }
+    }
+    console.log(str);
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
