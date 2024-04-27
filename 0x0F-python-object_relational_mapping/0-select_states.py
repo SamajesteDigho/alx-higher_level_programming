0e0103fb3d3d358d3d60fd95cfbc5d0ca2cb40de
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-27 15:49:43 +0800

    Exo 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,19 +8,22 @@ import MySQLdb
 
 
 args = sys.argv
-user = args[1]
-password = args[2]
-db_name = args[3]
-host = 'localhost'
-port = 3306
+if len(args) > 3:
+    user = args[1]
+    password = args[2]
+    db_name = args[3]
+    host = 'localhost'
+    port = 3306
 
-try:
-    db = MySQLdb.connect(host, user, password, "db_name")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    res = cur.fetchall()
-    for x in res:
-        print("{}".format(x))
-    db.close()
-except Exception as e:
-    print("{}".format(e))
+    try:
+        db = MySQLdb.connect(host, user, password, db_name)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states")
+        res = cur.fetchall()
+        for x in res:
+            print("{}".format(x))
+        db.close()
+    except Exception as e:
+        print("{}".format(e))
+else:
+    print("Arguments are insufficient")
