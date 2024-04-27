4c20a528b19cc7b3c9d7ff78ede03393f464efb9
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-28 06:43:16 +0800

    Exo 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+    Here the base model for state
+    Here the code and rule
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
