a3d10afbf9c1052c388dfdd6d749cfa504b9084d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-04-27 16:01:19 +0800

    Exo 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+    The module file for the Exo 1
+    Here we define the rules
+"""
+import sys
+import MySQLdb
+
+
+args = sys.argv
+if len(args) > 3:
+    user = args[1]
+    password = args[2]
+    db_name = args[3]
+    host = 'localhost'
+    port = 3306
+
+    try:
+        db = MySQLdb.connect(host, user, password, db_name)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states")
+        res = cur.fetchall()
+        for x in res:
+            if x[1][0] == 'N':
+                print("{}".format(x))
+        db.close()
+    except Exception as e:
+        print("{}".format(e))
+else:
+    print("Arguments are insufficient")
