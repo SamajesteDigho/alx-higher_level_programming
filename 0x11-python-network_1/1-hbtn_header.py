73eb5955769fa40dfc17d5251c859e7489853783
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-11 10:19:58 +0800

    Exo 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,3 +2,11 @@
 """
     Here the documentation of the module
 """
+import sys
+from urllib.request import urlopen
+
+
+if len(sys.argv) > 1:
+    with urlopen(sys.argv[1]) as response:
+        id = response.headers["X-Request-Id"]
+    print(id)
