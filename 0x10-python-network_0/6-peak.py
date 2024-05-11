014ebec47f63fcff6b645e16ac2910af5c48ce12
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-11 08:36:26 +0800

    Exo 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Peak finder module """
+
+
+def find_peak(list_of_integers):
+    """ Finding the pick out of the list"""
+    ls = list_of_integers
+    if len(ls) == 0:
+        return None
+    if 0 < len(ls) < 3:
+        return ls[0]
+    i = 1
+    while i < len(ls) - 1:
+        if ls[i-1] <= ls[i] >= ls[i+1] or ls[i-1] >= ls[i] <= ls[i+1]:
+            return ls[i]
+        i += 1
+    return ls[i]
