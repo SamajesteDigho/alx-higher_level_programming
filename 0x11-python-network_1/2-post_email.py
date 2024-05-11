4cdb3fb5efe96b4f325b4336cb5f839a354e2c06
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-11 10:39:13 +0800

    Exo 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,3 +2,16 @@
 """
     Here the documentation of the module
 """
+import sys
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+
+
+data = {"email": sys.argv[2]}
+data_enc = urlencode(data).encode("utf-8")
+request = Request(sys.argv[1], data=data_enc)
+request.add_header("Content-Type", "application/x-www-form-urlencode")
+with urlopen(request) as response:
+    body = response.read()
+
+print(body.decode())
