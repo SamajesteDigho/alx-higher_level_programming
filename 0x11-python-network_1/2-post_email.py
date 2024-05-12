0544989b2a1820313cbd17eb6c66e7ed517447b5
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-13 07:07:39 +0800

    Exo 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,13 +7,14 @@ from urllib.request import urlopen, Request
 from urllib.parse import urlencode
 
 
-data = {"email": sys.argv[2]}
-data_enc = urlencode(data).encode("utf-8")
-request = Request(sys.argv[1], data=data_enc, method="POST")
-request.add_header("Content-Type", "application/x-www-form-urlencode")
-try:
-    with urlopen(request) as response:
-        body = response.read()
-    print(body.decode())
-except Exception as e:
-    print("{}".format(e))
+if __name__ == "__main__":
+    data = {"email": sys.argv[2]}
+    data_enc = urlencode(data).encode("utf-8")
+    request = Request(sys.argv[1], data=data_enc, method="POST")
+    request.add_header("Content-Type", "application/x-www-form-urlencode")
+    try:
+        with urlopen(request) as response:
+            body = response.read()
+        print(body.decode())
+    except Exception as e:
+        print("{}".format(e))
