f752a4820325a665678e45b77ece660d9fe9cd70
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-13 07:17:07 +0800

    Exo 1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,10 +8,11 @@ from urllib.parse import urlencode
 
 
 if __name__ == "__main__":
-    data = {"email": sys.argv[2]}
-    data_enc = urlencode(data).encode("utf-8")
-    request = Request(sys.argv[1], data=data_enc, method="POST")
-    request.add_header("Content-Type", "application/x-www-form-urlencode")
+    # data = {"email": sys.argv[2]}
+    # data_enc = urlencode(data).encode("utf-8")
+    url = "{}?{}".format(sys.argv[1], sys.argv[2])
+    request = Request(sys.argv[1], method="POST")
+    # request.add_header("Content-Type", "application/x-www-form-urlencode")
     try:
         with urlopen(request) as response:
             body = response.read()
