edef77b8e9257d1710ef6e562be3a071e62b5b7d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-13 07:49:35 +0800

    Exo 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,3 +2,11 @@
 """
     Here the documentation of the module
 """
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.content.decode("utf-8"))))
+    print("\t- content: {}".format(response.content.decode("utf-8")))
