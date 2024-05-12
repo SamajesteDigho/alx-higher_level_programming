a004fdb920646dab8d13f9066f0d277a01bc9e24
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-13 07:54:26 +0800

    Exo 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,3 +2,10 @@
 """
     Here the documentation of the module
 """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get("{}".format(sys.argv[1]))
+    print("{}".format(response.headers["X-Request-Id"]))
