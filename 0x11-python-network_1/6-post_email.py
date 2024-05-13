91ee7f5b2e462664e56fb58189dd6cc818c134e3
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-13 08:01:56 +0800

    Exo 5

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,3 +2,10 @@
 """
     Here the documentation of the module
 """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.post(url=sys.argv[1], data={"email": sys.argv[2]})
+    print(response.content)
