eb5b69ebe90056651b6d7492bc13e76da119d06d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-13 08:13:08 +0800

    Exo 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,3 +2,13 @@
 """
     Here the documentation of the module
 """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get(url=sys.argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.content.decode("utf-8"))
