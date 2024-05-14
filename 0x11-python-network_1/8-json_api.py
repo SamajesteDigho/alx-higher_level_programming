f54e1238d148b85e2c9372dd5c29ce9f80c1c11e
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-05-14 23:31:55 +0800

    Exo 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,3 +2,21 @@
 """
     Here the documentation of the module
 """
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        data = {"q": sys.argv[1]}
+    else:
+        data = {"q": ""}
+    response = requests.post(url="http://0.0.0.0:5000/search_user", data=data)
+    try:
+        content = response.content.decode("utf-8")
+        if content is None or len(content.keys()) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(content["id"], content["name"]))
+    except Exception:
+        print("Not a valid JSON")
