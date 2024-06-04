7accbce83e7fad17b27562cafc51c1a6f2a592a9
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-05 07:36:45 +0800

    Exo 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,6 @@ const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 let data;
 let person;
-const listPerson = [];
 
 request(url, function (error, response, body) {
   if (error) {
@@ -16,7 +15,6 @@ request(url, function (error, response, body) {
     data.characters.forEach(actor => {
       request(actor, function (error, response, body) {
         if (error) {
-          console.log("Failed at id : " + actor);
           console.log(error);
         } else {
           person = JSON.parse(body);
