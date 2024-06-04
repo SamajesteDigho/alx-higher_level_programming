8688ada272f01b86cc009204300c2e61a3f97587
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-04 13:24:06 +0800

    Exo 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const epn = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + epn;
+
+fetch(url).then((response) => {
+  return response.json();
+}).then((data) => {
+  console.log(data.title);
+});
