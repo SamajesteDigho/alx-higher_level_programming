393995feec037d5754b5a921f42cade184ed1cf1
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-05 06:34:37 +0800

    Exo 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+let data;
+let person;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    data = JSON.parse(body);
+    data.characters.forEach(actor => {
+      request(actor, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          person = JSON.parse(body);
+          console.log(person.name);
+        }
+      });
+    });
+  }
+});
