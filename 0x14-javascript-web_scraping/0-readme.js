96200ce103168cf585ce272f272d237f310f8d4c
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-04 12:54:43 +0800

    Exo 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const path = process.argv[2];
+
+fs.readFile(path, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
