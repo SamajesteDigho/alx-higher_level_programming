e3b156bb55661633208d14dd10aee1c1759bc3e3
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-05 05:56:42 +0800

    Exo 2 to 4

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,15 @@
 #!/usr/bin/node
 
+const request = require('request');
 const epn = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + epn;
+let data;
 
-fetch(url).then((response) => {
-  return response.json();
-}).then((data) => {
-  console.log(data.title);
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 
+const request = require('request');
+
 const url = process.argv[2];
 const userId = 'https://swapi-api.alx-tools.com/api/people/18/';
 let count = 0;
-
-fetch(url).then((response) => {
-  return response.json();
-}).then((data) => {
-  data.results.forEach(film => {
-    if (film.characters.includes(userId)) {
-      count++;
-    }
-  });
-  console.log(count);
+let data;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    data = JSON.parse(body);
+    data.results.forEach(film => {
+      if (film.characters.includes(userId)) {
+        count++;
+      }
+    });
+    console.log(count);
+  }
 });
