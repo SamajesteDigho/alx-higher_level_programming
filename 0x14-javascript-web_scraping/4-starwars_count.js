ef9a0d1e2be56bbf6d4f55938c12c03a78dc194b
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-04 13:33:50 +0800

    Exo 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const url = process.argv[2];
+const userId = 'https://swapi-api.alx-tools.com/api/people/18/';
+let count = 0;
+
+fetch(url).then((response) => {
+  return response.json();
+}).then((data) => {
+  data.results.forEach(film => {
+    if (film.characters.includes(userId)) {
+      count++;
+    }
+  });
+  console.log(count);
+});
