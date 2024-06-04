2180fb78e0dcdb99b85e1ae6932fc5b9481a5dc7
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-05 06:22:53 +0800

    Exo 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+let data;
+const result = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    data = JSON.parse(body);
+    data.forEach(task => {
+      if (task.completed) {
+        if (result[task.userId]) {
+          result[task.userId]++;
+        } else {
+          result[task.userId] = 1;
+        }
+      }
+    });
+    console.log(result);
+  }
+});
