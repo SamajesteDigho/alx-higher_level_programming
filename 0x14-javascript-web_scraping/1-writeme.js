1674cf53d5c187d4923df02dc44214a2930f871a
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-06-04 13:05:06 +0800

    Exo 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+const path = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(path, text, (err) => {
+  if (err) throw console.log(err);
+});
